5a3a5dff5a7c2ca600c3983ad687c604180744ea
Josh Gough <jsgough@gmail.com>
2015-07-01 15:14:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,46 +1,5 @@
 var PORT = process.env.PORT || 9090;
 
-/* Example usage:
-
-To start:
-
-First:
-
-npm install
-npm start
-
-Second:
-
-You must POST the definition of a hook to the server at /hook. Example:
-
-  https://www7.v1host.com/V1Production/api/ActivityStream/Scope%3a653184
-
-It is the URL for where to read data from, coupled with the creds and protocol.
-
-The hookUrl should have the full address like:
-
-  http://requestb.in/o0uouoo0
-
-After reading from queryUrl, the server will turn around and POST the data it gets to hookUrl.
-
-queryInterval is in milliseconds. 
-
-Using cURL:
-
-curl -i -X POST \
-   -H "Content-Type:application/json" \
-   -d \
-'{
-  "queryCreds": "jogoshugh:SECRET",
-  "queryUrl": "www7.v1host.com/V1Production/api/ActivityStream/Scope%3a653184",
-  "queryProtocol": "https",
-  "queryInterval": 5000,
-  "hookUrl": "http://requestb.in/o0uouoo0"
- }' \
- 'http://localhost:9090/hook'
-
-*/
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
